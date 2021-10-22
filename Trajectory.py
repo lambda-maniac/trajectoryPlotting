dd652e1ced982d4d5c479b6b20871b330a59dc13
Sr. Meia-noite { λ } (LordZarkares) <73501736+LordZarkares@users.noreply.github.com>
2021-10-22 16:49:24 -0300

    v1.0.2 wip

--- Trajectory.py
+++ Trajectory.py
@@ -10,13 +10,14 @@ class Vector2:
 
 class Arc:
     def __init__(self, origin, angle, velocity, gravity, worldX, worldY):
-        self.origin   = origin
-        self.angle    = angle * pi / 180
-        self.rawAngle = angle
-        self.velocity = velocity
-        self.gravity  = -gravity
-        self.worldX   = worldX
-        self.worldY   = worldY
+        self.origin     = origin
+        self.angle      = angle * pi / 180
+        self.rawAngle   = angle
+        self.velocity   = velocity
+        self.gravity    = -gravity
+        self.rawGravity = gravity
+        self.worldX     = worldX
+        self.worldY     = worldY
 
     def trajectory(self, time):
         """
@@ -35,11 +36,25 @@ class Arc:
             (2 * (self.velocity ** 2) * (cos(self.angle) ** 2))
         ) + self.origin.y
 
-    def plot(self):
+    # Formula: v0²sin2θ / g
+    @property# It can be a property because there's no need to function in time.
+    def R(self): return ((self.velocity ** 2) * sin(2 * self.angle)) / self.rawGravity
+
+    # Formula: (v0²sin²θ / 2g) [+ h]
+    @property# It can be a property because there's no need to function in time.
+    def H(self): return (((self.velocity ** 2) * (sin(self.angle) ** 2)) / (2 * self.rawGravity)) + self.origin.y
+
+    def plotArc(self):
         xs = np.linspace(self.origin.x, self.worldX.y if self.rawAngle <= 90 else self.worldX.x)
         ys = self.trajectory(xs)
 
-        plt.plot(xs, ys)
+        plt.plot(xs, ys, "--g")
+
+    def plotRange(self):
+        plt.hlines(self.origin.y, self.origin.x, self.R, "y", "dashed")
+
+    def plotHeight(self):
+        plt.vlines(self.R / 2, self.origin.y, self.H, "r", "dashed")
 
 def main():
     # ========== \Setup ========== #
@@ -49,7 +64,7 @@ def main():
 
     # Some more constants:
     ANGLE    = 100
-    VELOCITY = 11.15
+    VELOCITY = 14.45
     GRAVITY  = 10
 
     # Draw Thrower:
@@ -57,7 +72,7 @@ def main():
     plt.scatter(THROWER.x, THROWER.y)
 
     # Draw a Target:
-    PSEUDO_TARGET1 = Vector2(X_LIM.x / 2, 0)
+    PSEUDO_TARGET1 = Vector2(X_LIM.x + 2, 0)
     plt.scatter(PSEUDO_TARGET1.x, PSEUDO_TARGET1.y)
 
     # Draw a Target:
@@ -77,10 +92,15 @@ def main():
 
     # ========== \Trajectory calculating ========== #
 
+    TRAJECTORY1 = Arc(THROWER, ANGLE, VELOCITY, GRAVITY, X_LIM, Y_LIM)
+    TRAJECTORY1.plotArc()
+    TRAJECTORY1.plotRange()
+    TRAJECTORY1.plotHeight()
 
-    TRAJECTORY = Arc(THROWER, ANGLE, VELOCITY, GRAVITY, X_LIM, Y_LIM)
-    TRAJECTORY.plot()
-
+    TRAJECTORY2 = Arc(THROWER, 80, 11.15, GRAVITY, X_LIM, Y_LIM)
+    TRAJECTORY2.plotArc()
+    TRAJECTORY2.plotRange()
+    TRAJECTORY2.plotHeight()
 
     # ========== /Trajectory calculating ========== #
 
