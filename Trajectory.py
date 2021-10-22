6ba7bd2eef4a91fd3c87954baa49eb37195f1b00
Sr. Meia-noite { λ } (LordZarkares) <73501736+LordZarkares@users.noreply.github.com>
2021-10-22 20:55:53 -0300

    v1.0.5

--- Trajectory.py
+++ Trajectory.py
@@ -22,8 +22,7 @@ class Arc:
     def trajectory(self, time):
         """
         ::: Formula:
-        :::     y = xtanθ + gx² / 2v0.cos²θ + h
-        :::
+        :::     y = xtan(θ) + (gx² / 2v0.cos²(θ)) + h
         ::: Where:
         :::     x is time.
         :::     v is the velocity, and v0 is velocity in function of time 0.
@@ -36,6 +35,9 @@ class Arc:
             (2 * (self.velocity ** 2) * (cos(self.angle) ** 2))
         ) + self.origin.y
 
+    @property
+    def isRight(self): return self.rawAngle <= 90
+
     # Formula: v0²sin2θ / g
     @property# It can be a property because there's no need to function in time.
     def R(self): return ((self.velocity ** 2) * sin(2 * self.angle)) / self.rawGravity
@@ -45,10 +47,24 @@ class Arc:
     def H(self): return (((self.velocity ** 2) * (sin(self.angle) ** 2)) / (2 * self.rawGravity)) + self.origin.y
 
     def plotArc(self):
-        xs = np.linspace(self.origin.x, self.worldX.y if self.rawAngle <= 90 else self.worldX.x)
+        xs = np.linspace(self.origin.x, self.worldX.y if self.isRight else self.worldX.x)
         ys = self.trajectory(xs)
 
         plt.plot(xs, ys, "--g")
+
+    def plotTangents(self):
+
+        ts = np.linspace(self.origin.x, self.worldY.y)
+
+        xs = (lambda x: cos(self.angle) * x                    )(ts)
+        ys = (lambda x: sin(self.angle) + self.origin.y + x - 1)(ts)
+        plt.plot(xs, ys, "--b")
+
+        # xs = (lambda x: cos(self.angle) * x * 2                )(ts)
+        # ys = (lambda x: sin(self.angle) + self.origin.y + x - 1)(ts)
+        # plt.plot(xs, ys, "--b")
+
+        plt.hlines(self.H, self.R / 2, self.worldX.y if self.isRight else self.worldX.x, "b", "dashed")
 
     def plotRange(self):
         plt.hlines(self.origin.y, self.origin.x, self.R, "y", "dashed")
@@ -96,11 +112,13 @@ def main():
     TRAJECTORY1.plotArc()
     TRAJECTORY1.plotRange()
     TRAJECTORY1.plotHeight()
+    TRAJECTORY1.plotTangents()
 
     TRAJECTORY2 = Arc(THROWER, 80, 11.15, GRAVITY, X_LIM, Y_LIM)
     TRAJECTORY2.plotArc()
     TRAJECTORY2.plotRange()
     TRAJECTORY2.plotHeight()
+    TRAJECTORY2.plotTangents()
 
     # ========== /Trajectory calculating ========== #
 
